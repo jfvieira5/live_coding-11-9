623d2f3646289004149eb5cc5996ae1ed608ae24
jfvieira5 <jefferson.jose@ufac.br>
2022-12-18 17:57:20 -0500

    2

---
+++ git2.R
@@ -0,0 +1,19 @@
+usethis::use_git_config(
+  user.name = "jfvieira5",
+  user.email = "jefferson.jose@ufac.br")
+
+#criar token
+usethis::create_github_token()
+
+#Abrir r environ
+usethis::edit_r_environ(scope = "project")
+
+#avaliaçao da situção
+usethis::git_sitrep()
+
+#iniciar git
+usethis::use_git()
+
+usethis:git_sitrep()
+
+
